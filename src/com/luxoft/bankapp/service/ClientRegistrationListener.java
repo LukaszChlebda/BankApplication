a45537085842d8a5f1b186054cfcdf1b26fc7daf
LChlebda <lukaszchlebda>
2016-01-15 14:44:47 +0100

    Added maven support

--- src/com/luxoft/bankapp/service/ClientRegistrationListener.java
+++
@@ -1,7 +0,0 @@
-package com.luxoft.bankapp.service;
-
-import com.luxoft.bankapp.model.Client;
-
-public interface ClientRegistrationListener {
-	public void onClientAdded(Client c);
-}
--- src/com/luxoft/bankapp/service/Report.java
+++
@@ -1,6 +0,0 @@
-package com.luxoft.bankapp.service;
-
-public interface Report {
-	public void printReport();
-
-}
